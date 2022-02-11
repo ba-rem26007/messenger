8cc57433722e2635bc7f91a6265ca83c9f1cbf40
elrems <dev@exemple.com>
2022-02-11 21:33:50 +0100

    add user in migration

--- migrations/Version20220210134818.php
+++ migrations/Version20220210134818.php
@@ -21,6 +21,17 @@ final class Version20220210134818 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, nom VARCHAR(255) NOT NULL, prenom VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+		
+		
+		$this->addSql(
+            '
+            INSERT INTO `user` (`username`, `roles`, `password`,`nom`,`prenom`)
+            VALUES
+                (\'admin@fr.fr\', \'["ROLE_ADMIN"]\', \'$2y$13$fNk/atKuVqQVi.oIlEKbEeCxgiQseRih64zmQ9IBRerssLRlogN1m\',
+				'admin' , 'admin');
+            '
+        );
+		
     }
 
     public function down(Schema $schema): void
