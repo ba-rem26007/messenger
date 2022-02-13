61974780f331d8a8dc967781b76597b0db4835b7
elrems <dev@exemple.com>
2022-02-13 11:03:06 +0100

    modifie le comportement pour le cron

--- src/Form/MessageType.php
+++ src/Form/MessageType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Message;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -13,17 +15,19 @@ class MessageType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        // https://symfony.com/doc/current/reference/forms/types.html
         $builder
-            ->add('Title')
-            ->add('Body')
+            ->add('Title', TextType::class, ['label' => 'Titre :'])
+            ->add('Body', TextareaType::class, ['label' => 'Contenu du message :'])
             ->add('EmissionDate', DateTimeType::class, [
                 'widget' => 'single_text',
+                'data' => new \DateTime("now")
                 //'html5' => false,
             ])
             //->add('Status')
             //->add('SendingDate')
             ->add('Choice', ChoiceType::class, [
-                'label' => 'Type:',
+                'label' => 'Type de channel :',
                 'choices' => Message::CHOICES,
                 //'expanded' => true,
                 //'mapped' => false
